bf552052f3641d12ee1a91d7151b598520f2a2d4
dez1023 <123430228+dez1023@users.noreply.github.com>
2025-09-30 14:59:05 -0700

    Create camera.js

---
+++ src/SBM/camera.js
@@ -0,0 +1,33 @@
+class Camera extends GameObject {
+  fov = 70;
+  mode = 1;
+  _view3D = false;
+  subjects = [];
+
+  constructor(position, rotation) {
+    super(position);
+    this.rotation = rotation ?? new Vector3();
+    this.view3D = false;
+  }
+
+  get view3D() {
+    return _view3D;
+  }
+
+  set view3D(newValue) {
+    this._view3D = newValue;
+    root.style.setProperty("--face-display", newValue ? "inline" : "hidden");
+  }
+
+  frameUpdate() {
+    if (this.subjects.length > 0) {
+      switch (this.mode) {
+        case 2:
+          this.position = this.subjects[0].position;
+          break;
+      }
+    }
+    body.style.perspective = 7800 - this.fov * 100 + "px";
+    body.style.transform = `translate3d(${this.position.x}px, ${this.position.y}px, ${this.position.z}px)`;
+  }
+}
